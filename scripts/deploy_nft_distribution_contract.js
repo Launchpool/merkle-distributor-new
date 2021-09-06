7762ee7ec77b3ee320c6b06fcc022470b9bb0219
Keiron <keiron.gulrajani@gmail.com>
2021-09-06 13:39:18 +0100

    updated nft deployment script added generated claims added nft json

--- scripts/deploy_nft_distribution_contract.js
+++ scripts/deploy_nft_distribution_contract.js
@@ -1,19 +1,23 @@
 const prompt = require('prompt-sync')()
-const ERC1155Metadata = require('../artifacts/contracts/test/TestERC1155.sol/TestERC1155.json')
-
+const ERC1155Metadata = require('../abi/LaunchPoolErc1155.json')
+const LPOOL_ERC1155_ADDRESS = '0xDE508B1313367309360ddd5F0F416c42102a0eEf'
+const LPOOL_ERC1155_BSC_ADDRESS = '0x7A3c1a95fF81819eAdB5b8D9Ca0D86891eD340Ed'
 async function main () {
   const [deployer] = await ethers.getSigners()
   console.log('Deploying staking contract with guild using the account:', await deployer.getAddress())
 
-  const tokenAddress = prompt('ERC1155 address? ') // 0x22acaee85ddb83a3a33b7f0928a0e2c3bfdb6a4f
-  const merkleRoot = prompt('Merkle root? ') // 0x9d9f8634db1902d10a2615ece779461a69e8fcc57749652d3f8663237c562ede copy from generated json
-  const tokenTotals = { '0x00': '0x06', '0x01': '0x01' }
+  const tokenAddress = prompt('ERC1155 address? ') // 0x7A3c1a95fF81819eAdB5b8D9Ca0D86891eD340Ed
+  const merkleRoot = prompt('Merkle root? ') // 0x2758d19f248fbfd16ff987f9da0ad10a909655f7d60dce02084416e62e9a3226 copy from generated json
+  const tokenId = prompt('Token Id? ') // 0x02
+  const amount = prompt('Amount to mint? ') // 0x10ed
 
   const tokenInstance = new ethers.Contract(tokenAddress, ERC1155Metadata.abi, deployer)
 
   console.log('ERC1155 address:', tokenAddress)
 
   console.log('\nMerkle root:', merkleRoot)
+  console.log('\nToken Id:', tokenId)
+  console.log('\nAmount:', amount)
 
   prompt('If happy, hit enter...')
 
@@ -25,27 +29,13 @@ async function main () {
 
   console.log('Claim contract deployed at', merkleDistributorInstance.address)
 
-  const tokenIds = []
-  const amounts = []
-  for (const tokenId in tokenTotals) {
-    tokenIds.push(tokenId)
-    amounts.push(tokenTotals[tokenId])
-
-    console.log(`${ethers.BigNumber.from(tokenId)}: ${ethers.BigNumber.from(tokenTotals[tokenId])}`)
-  }
-  console.log('About to send amount of tokens to claim contract')
+  console.log('About to mint tokens to claim contract')
 
   prompt('\nIf happy, hit enter...\n')
 
   const deployerAddress = await deployer.getAddress()
-  console.log('\nMoving tokens from ' + deployerAddress + ' to ' + merkleDistributorInstance.address)
-  const tx = await tokenInstance.safeBatchTransferFrom(
-    deployerAddress,
-    merkleDistributorInstance.address,
-    tokenIds,
-    amounts,
-    0x0
-  )
+  console.log('\nMinting tokens from ' + deployerAddress + ' to ' + merkleDistributorInstance.address)
+  const tx = await tokenInstance.mint(merkleDistributorInstance.address, tokenId, amount)
   await tx.wait()
 
   console.log('Done!')
